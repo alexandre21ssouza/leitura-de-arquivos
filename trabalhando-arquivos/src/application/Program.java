04e0f4d7c206c777dd80000abc20dbc382b2dab3
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-11-13 18:38:42 -0300

    Uso do modelo FileReader e BufferedReader para manipulação de arquivos

--- trabalhando-arquivos/src/application/Program.java
+++ trabalhando-arquivos/src/application/Program.java
@@ -1,8 +1,14 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+
+/*
+ * Comentários são para fins didáticos.
+ */
 
 public class Program {
 
@@ -28,6 +34,48 @@ public class Program {
 			sc.close();
 			}
 		}
+		
+		//inserindo aqrquivos de outra maneira:
+		String path = "C:\\Users\\Alexandre\\Documents\\apresentação";
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			//Faz a leitura do arquivo.
+			fr = new FileReader(path);
+			
+			//Recebe variável(fr) para deixar a leitura mais rápida.
+			br = new BufferedReader(fr);
+			
+			/*Faz a leitura de uma linha do arquivo(br) e vai para o teste
+			 * While, caso seja linha única, não lê mais nenhuma pois será
+			 * valor null, caso tenha mais linhas após esta, será diferente 
+			 * de null e entra no laço While atá ser null.
+			 */
+			String line = br.readLine();
+			
+			//Laço while
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+		} catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+			
+		} finally {
+			try {
+			if(br != null) {
+				br.close();
+			}
+			if(fr != null) {
+				fr.close();
+			}
+		} catch(IOException e) {
+			//Mostra o erro caso tenha alguma exception.
+			e.printStackTrace();
+		}
+			
+	  }
 	}
-
 }
